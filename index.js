15d5335a79cb6eb25fc8c51fd56a6899df8b2824
Glen <glencbz@gmail.com>
2016-03-12 21:10:13 +0100

    accepting post requests to /sales

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-import express from 'express'
-import morgan from 'morgan'
-import routes from './routes'
+import express from 'express';
+import morgan from 'morgan';
+import routes from './routes';
+import cors from 'cors';
+
+var bodyParser = require('body-parser');
 
 console.log('Server online')
 express()
-  // .use(cors)
+  .use(cors())
   .use(morgan('dev'))
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({
+    extended: true
+  }))
   .use('/', routes)
-  .listen(4005)
+  .listen(4005);
--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import mysql from 'mysql'
+import mysql from 'mysql' 
 
 const router = express.Router()
 const connection = mysql.createConnection({
@@ -21,9 +21,9 @@ router.get('/stats/gender', (req, res) => {
   console.log(req.query)
   var itemID = req.query.id
 
-  query = "select *, sales_m/sales_total as m_p, sales_f/sales_total as f_p from (select (select count(*) from sales where gender = 'f' and itemName = ?) as sales_f,
+  query = `select *, sales_m/sales_total as m_p, sales_f/sales_total as f_p from (select (select count(*) from sales where gender = 'f' and itemName = ?) as sales_f,
 (select count(*) from sales where gender = 'm' and itemName = ?) as sales_m,
-(select count(*) from sales where itemName = ?) as sales_total) as gender_stats;"
+(select count(*) from sales where itemName = ?) as sales_total) as gender_stats;`;
   connection.query(query,[itemID,itemID,itemID], function(err, rows, fields) {
       if (err) throw err;
       responseMessage.stats = rows[0];
@@ -42,12 +42,12 @@ router.get('/stats/age', (req, res) => {
   console.log(req.query)
   var item = req.query.item
 
-  query = "select *, sales_age_u18/sales_total, sales_age_u30/sales_total, sales_age_u50/sales_total, sales_age_o50/sales_total,
+  query = `select *, sales_age_u18/sales_total, sales_age_u30/sales_total, sales_age_u50/sales_total, sales_age_o50/sales_total,
 sales_total as p_age_u18 from (select (select count(*) from sales where itemName = ? and age <= 18) as sales_age_u18, 
 (select count(*) from sales where itemName = ? and age > 18 and age <= 30) as sales_age_u30, 
 (select count(*) from sales where itemName = ? and age > 31 and age <= 50) as sales_age_u50, 
 (select count(*) from sales where itemName = ? and age > 50) as sales_age_o50, 
-(select count(*) from sales where itemName = ?) as sales_total) as sales_temp;"
+(select count(*) from sales where itemName = ?) as sales_total) as sales_temp;`;
   connection.query(query,[item,item,item,item,item], function(err, rows, fields) {
       if (err) throw err;
       responseMessage.stats = rows[0];
@@ -58,27 +58,27 @@ sales_total as p_age_u18 from (select (select count(*) from sales where itemName
   // run optimizer here
   // res.sendStatus(200)
 
-})
+});
 
 router.post('/sales', (req, res) => {
   // query params
-  console.log(req.body)
+  console.log(req.body);
   var item = req.body.item;
   var age = req.body.buyer.age;
   var gender = req.body.buyer.gender;
   var mood = req.body.buyer.mood;
 
-  connection.query('insert into sales values (DEFAULT,?,?,?,?,DEFAULT)', [item, age, gender, mood], function(err, results) {
-  if (err) throw err;
-  console.log('Sales submitted!');
-
+  connection.query('insert into sales values (DEFAULT,?,?,?,?,DEFAULT)', [item, age, gender, mood], 
+    function(err, results) {
+     if (err) throw err;
+      console.log('Sales submitted!');
   });
 
   // process data here
   // run optimizer here
 
-  res.sendStatus(200)
+  res.sendStatus(200);
 
-})
+});
 
 export default router
